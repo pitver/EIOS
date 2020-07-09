2f7137ad82d0fc3bb40d8b2bb72f0749766fa446
Vadim Shakirov <vadim.shakirov@rtlabs.ru>
2020-07-09 19:53:40 +0300

    init

---
+++ src/main/java/ru/stc23/eios/EiosApplication.java
@@ -0,0 +1,13 @@
+package ru.stc23.eios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EiosApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EiosApplication.class, args);
+  }
+
+}
