0f6e0e2deffe3fb75fbd1362f2c7a46ea436f8ac
Dayanova <fayagul.galieva@mail.ru>
2020-07-13 23:14:58 +0300

    create Review class model

---
+++ src/main/java/ru/stc23/eios/model/Review.java
@@ -0,0 +1,68 @@
+package ru.stc23.eios.model;
+
+import java.util.Date;
+
+/**
+ * @author Даянова Фаягуль
+ */
+
+public class Review {
+
+    private Long id;
+    private int work;
+    private String state_id;
+    private int user;
+    private String text;
+    private Date create_date;
+
+    public Review() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getWork() {
+        return work;
+    }
+
+    public void setWork(int work) {
+        this.work = work;
+    }
+
+    public String getState_id() {
+        return state_id;
+    }
+
+    public void setState_id(String state_id) {
+        this.state_id = state_id;
+    }
+
+    public int getUser() {
+        return user;
+    }
+
+    public void setUser(int user) {
+        this.user = user;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(Date create_date) {
+        this.create_date = create_date;
+    }
+}
