6d2bb7950ccb22138085ab760348e614f9231573
Petr Vershinin <pitver2014@yandex.ru>
2020-07-17 16:59:57 +0300

    EIOS-1111-test version

---
+++ src/main/java/ru/stc23/eios/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package ru.stc23.eios.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.util.Properties;
+
+
+/**
+ * SecurityConfig
+ *
+ * @author Вершинин Пётр
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
+
+    // роль admin всегда есть доступ к /admin/**
+    // роль user всегда есть доступ к /user/**
+    // Наш кастомный "403 access denied" обработчик
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable().httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/index", "/about","/register").permitAll()
+                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("USER","ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+    }
+
+    // создаем пользоватлелей, admin и user
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(inMemoryUserDetailsManager());
+
+    }
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        final Properties users = new Properties();
+        users.put("user","1,ROLE_USER,enabled");
+        users.put("admin","1,ROLE_ADMIN,enabled");
+        return new InMemoryUserDetailsManager(users);
+    }
+}
+
+
---
+++ src/main/java/ru/stc23/eios/controller/MainController.java
@@ -0,0 +1,74 @@
+package ru.stc23.eios.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * MainController
+ *
+ * @author Вершинин Пётр
+ */
+@Controller
+@EnableWebSecurity
+public class MainController {
+
+    private final InMemoryUserDetailsManager inMemoryUserDetailsManager;
+
+    @Autowired
+    public MainController(InMemoryUserDetailsManager inMemoryUserDetailsManager) {
+        this.inMemoryUserDetailsManager = inMemoryUserDetailsManager;
+    }
+
+    @GetMapping(value = {"/", "/index"})
+    public String index() {
+        return "/index";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "/admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "/user";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "/about";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "/register";
+    }
+
+    @PostMapping("/register")
+    public String addUser(
+            @RequestParam("username") String name,
+            @RequestParam("password") String password/*,
+            @RequestParam("email") String email*/
+    ) {
+
+            inMemoryUserDetailsManager.createUser(User.withUsername(name).password(password).roles("USER").build());
+
+        return "/login";
+    }
+
+    @GetMapping("/403")
+    public String error403() {
+        return "/error/403";
+    }
+}
