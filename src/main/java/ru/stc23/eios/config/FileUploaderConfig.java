9378c6aa9b6a8e9514628dd097a544c83cdcba94
Vasiliy Matveev <bazilmat@yandex.ru>
2020-07-26 06:57:46 +0300

    File Upload model create

---
+++ src/main/java/ru/stc23/eios/config/FileUploaderConfig.java
@@ -0,0 +1,29 @@
+package ru.stc23.eios.config;
+
+/**
+ * @author Matveev
+ */
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class FileUploaderConfig {
+
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.parse("5MB"));
+        factory.setMaxRequestSize(DataSize.parse("5MB"));
+        return factory.createMultipartConfig();
+    }
+
+}
