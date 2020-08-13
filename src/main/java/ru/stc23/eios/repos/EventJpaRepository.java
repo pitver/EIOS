f91d4278da471a8bc18c7a7e6a17cc70a0238b0c
Dayanova <fayagul.galieva@mail.ru>
2020-08-14 00:05:40 +0300

    work calendar

--- src/main/java/ru/stc23/eios/repos/EventJpaRepository.java
+++ src/main/java/ru/stc23/eios/repos/EventJpaRepository.java
@@ -4,16 +4,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.stc23.eios.model.Event;
+import ru.stc23.eios.model.Student;
+import ru.stc23.eios.model.User;
+
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface  EventJpaRepository extends JpaRepository<Event, Long> {
+    List<Event> findAll();
+    Event save(Event event);
+    void delete(Event event);
 
-   @Query("select b from Event b where b.startDateTime >= ?1 and b.endDateTime <= ?2")
-  public List<Event> findByDateBetween(LocalDateTime start, LocalDateTime end);
+   @Query("select b from Event b where b.startDateTime >= ?1 and b.endDateTime <= ?2 and b.author= ?3" )
+  public List<Event> findByDateBetween(LocalDateTime start, LocalDateTime end, Student user);
 
+    List<Event> findByAuthor(User user);
 
 
 }
